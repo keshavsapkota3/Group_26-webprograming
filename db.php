3b3bf4b4df871b792f77dd911ee12d1e21d99378
ZyAnC <yanzheng1214@outlook.com>
2024-02-15 12:01:44 +0200

    yz

--- db.php
+++ db.php
@@ -1,8 +1,15 @@
 <?php
+<<<<<<< HEAD
 $servername = "localhost"; // Replace with your MySQL server hostname
 $username = "bbcap23_26";     // Replace with your MySQL username
 $password = "Is73JdUf";     // Replace with your MySQL password
 $dbname = "wp_bbcap23_26";       // Replace with the name of your MySQL database
+=======
+$servername = "db"; // Replace with your MySQL server hostname
+$username = "team26";     // Replace with your MySQL username
+$password = "bbcap23_26";     // Replace with your MySQL password
+$dbname = "team26";       // Replace with the name of your MySQL database
+>>>>>>> 262662f112d6c5f0d2def6b672a71d141da03aa2
 
 // Create a database connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -12,3 +19,4 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+<?php
